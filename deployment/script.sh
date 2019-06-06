549a34a1cc24d70aab8c07386f9c30701617e16a
Christophe <c.lejeune@free.fr>
2019-06-06 08:01:39 +0000

    Add stage build

--- deployment/script.sh
+++ deployment/script.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 echo 'Hello world'
 echo 'The environment is ${ENVIRONMENT}'
+echo 'The environment is {ENVIRONMENT}'
+echo "The environment is ${ENVIRONMENT}"
+echo 'The environment is ${env.ENVIRONMENT}'
+echo 'The environment is {env.ENVIRONMENT}'
+echo "The environment is ${env.ENVIRONMENT}"
